a39dad0f0a7bce4520c11ea475b090b8be412172
tkemaladze98 <tkemaladze98>
2024-02-08 11:07:17 +0000

    System generated commit, initial commit

---
+++ pages/Page1/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,20 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	test :true,
+	data:[{
+		name:'shota',
+		surname:'tkemaladze',
+		age:25,
+		sex:'male'
+	}],
+	myFun1 () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+		console.log('askaslhgsalhgla')
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
